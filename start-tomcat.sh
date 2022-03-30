625e30a6ef7ab1855c47bb001c98635a78173840
Leonardo De Faveri <leonardo.defaveri01@gmail.com>
2022-03-30 13:59:46 +0200

    Removed personal path

--- start-tomcat.sh
+++ start-tomcat.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
 
+data_dir="Your dir"
+
 docker run --detach -ti -p 8080:8080 -p 22:22 --name tomcat \
-	-v /media/leonardo/various/esercizi-uni/prog-web/data:/data \
+	-v ${data_dir}:/data \
 	--network=tomcat-docker_xampp-tomcat -e LANG=C.UTF-8 \
 	my/tomcat
